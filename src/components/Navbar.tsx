c3672de8c286a8bb8824097a1951cd597e7fbdd3
Nuthanan Tharmarajah <n2tharma@uwaterloo.ca>
2025-09-30 20:57:47 -0400

    added schedule

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -173,7 +173,7 @@ export default function Navbar() {
         </ul>
         {/* Nav Links */}
         <ul
-          className={`hidden md:flex lg:gap-10 md:text-lg md:gap-4 lg:text-xl  font-dm-sans font-light items-center mr-6`}
+          className={`hidden md:flex lg:gap-10 md:text-[14px] md:gap-4 lg:text-lg  font-dm-sans font-light items-center mr-6`}
         >
           {navItems.map((item) => (
             <li
