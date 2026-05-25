4ef655bc9b492e423610bcb64224857b72798e05
Nuthanan Tharmarajah <tharmarajahnuthanan@gmail.com>
2026-05-25 19:10:48 -0400

    added something

--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -456,7 +456,7 @@ export const secretariat = [
     branch: "Internal",
   },
   {
-    name: "Abraar Hussain",
+    name: "Abraar Hussai",
     position: "ASG, Fictional",
     image: `${CF_DOMAIN}/abraar.jpg?format=webp`,
     branch: "Internal",
