21ddb5c54c8ee1cb6ab6aab629386152f289cb29
Nuthanan Tharmarajah <nuthanan06@gmail.com>
2026-08-26 00:15:42 -0400

    fixed lag

--- src/app/committees/page.tsx
+++ src/app/committees/page.tsx
@@ -1,10 +1,11 @@
-import { Suspense } from "react";
 import Committees from "@/views/committees";
 
-export default function Page() {
-  return (
-    <Suspense fallback={null}>
-      <Committees />
-    </Suspense>
-  );
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) {
+  const { filter } = await searchParams;
+
+  return <Committees initialFilter={filter || "All"} />;
 }
